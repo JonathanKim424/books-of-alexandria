9c9592b2cd715c6abe7f4d8410fccad3aed4cbc6
Stebbi <stefodd@yahoo.com>
2022-05-10 22:58:34 -0500

    initial genre routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const bookRoutes = require('./book-routes');
+const genreRoutes = require('./genre-routes');
 
 router.use('/books', bookRoutes);
+router.use('/genres', genreRoutes);
 
 module.exports = router;
