fa156bd5d99d87c1e5362676e2646483d59bad10
Stebbi <stefodd@yahoo.com>
2022-05-14 20:04:27 -0500

    rendered book db info onto inventory page

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
+const inventoryRoutes = require('./inventory-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/inventory', inventoryRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
---
+++ controllers/inventory-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Book, Genre } = require('../models');
+
+router.get('/', (req, res) => {
+    Book.findAll({
+        attributes: ['id', 'book_title', 'genre_id', 'author', 'price', 'stock'],
+        include: [
+            {
+                model: Genre,
+                attributes: ['genre_name']
+            }
+        ] 
+    })
+    .then(dbBookData => {
+        const books = dbBookData.map(book => book.get({ plain: true }));
+        console.log(books)
+        res.render('inventory', { books/*, loggedIn: true*/ });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
